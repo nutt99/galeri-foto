a1ba0bdbb2e37b95cdd9be30625386c47a967cd4
nutt99 <NataGaming766@gmail.com>
2024-01-06 08:58:06 +0700

    upload project

---
+++ resources/views/login.blade.php
@@ -0,0 +1,13 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Bootstrap demo</title>
+    <link href="bootstrap/css/bootstrap.min.css" type="text/css" rel="stylesheet">
+  </head>
+  <body>
+    <h1 class="text-primary">Hello, world!</h1>
+    <script src="bootstrap/js/bootstrap.bundle.min.js"></script>
+  </body>
+</html>
